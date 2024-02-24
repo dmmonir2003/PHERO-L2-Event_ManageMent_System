8420569bba06171b87cbec1ac8154a52d5b72e0b
dmmonir2003 <dmmonir2003@gmail.com>
2024-02-24 23:50:32 +0600

    setup for deploy

--- EventManagementSystem/settings.py
+++ EventManagementSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +30,15 @@ ALLOWED_HOSTS = ["*"]
 
 APPEND_SLASH = False
 # Application definition
+
+# vercell-setting add
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+
 
 INSTALLED_APPS = [
     'rest_framework',
@@ -138,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
